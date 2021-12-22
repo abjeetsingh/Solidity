6f543ec78dcb07d6bca3aa878f1e44f5fecbb669
Abjeet Singh <74428453+abjeetsingh@users.noreply.github.com>
2021-12-22 17:12:13 +0530

    Create StorageFactory.sol

---
+++ StorageFactory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.6.6;
+
+import "./simpleStorage.sol"; 
+
+contract StorageFactory is simpleStorage{
+
+    simpleStorage[] public simpleStorageArray;
+    
+    function makeSimpleStorage() public{
+        simpleStorage simplestorage = new simpleStorage();
+        simpleStorageArray.push(simplestorage);
+    }
+    function SimpleFacrtoryStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public{
+        simpleStorage simplestorage = simpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simplestorage.enterNumber(_simpleStorageNumber);
+    }
+
+    function SFGet(uint256 _simpleStorageIndex) view public returns(uint256){
+        simpleStorage simplestorage = simpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simplestorage.retriveNumber();
+    }
+}
